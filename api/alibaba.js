8e9bf7b09c88ff0b847b1922d949458b701a433c
rafiai134 <rafiullah6khan@gmail.com>
2026-09-25 01:34:55 +0500

    Add handler for Alibaba API authorization check

--- api/alibaba.js
+++ api/alibaba.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
 
+  return res.status(200).json({
+    connected: false,
+    status: "Alibaba API connection is waiting for authorization",
+    message:
+      "Alibaba.com Open Platform authorization is required before live supplier/product data can be accessed."
+  });
+}
